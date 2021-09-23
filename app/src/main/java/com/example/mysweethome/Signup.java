5b0ab000554777336af90a5a6246ef57da94a4bb
rawan ahmad fawzi khasawneh <rawanahmed115@gmail.com>
2021-09-24 00:35:20 +0300

    cokoring

--- app/src/main/java/com/example/mysweethome/Signup.java
+++ app/src/main/java/com/example/mysweethome/Signup.java
@@ -29,21 +29,21 @@ public class Signup extends AppCompatActivity {
         private Button signUpBtn ;
         private Handler toastHandler ;
         ImageView imageView;
-        Animation bottom;
+        Animation top;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_signup);
 
-            bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+            top = AnimationUtils.loadAnimation(this, R.anim.top_animation);
             imageView = findViewById(R.id.imag2);
             username = findViewById(R.id.username);
             email = findViewById(R.id.emailRegister);
             password = findViewById(R.id.passwordRegister);
             signUpBtn = findViewById(R.id.save);
 
-            imageView.setAnimation(bottom);
+            imageView.setAnimation(top);
 
 
             signUpBtn.setOnClickListener(new View.OnClickListener(){
