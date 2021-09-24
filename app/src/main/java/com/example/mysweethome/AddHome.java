1a8f8fe93d9be614ec582bd529514c58551378c7
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-25 00:34:01 +0300

    starting array of images

--- app/src/main/java/com/example/mysweethome/AddHome.java
+++ app/src/main/java/com/example/mysweethome/AddHome.java
@@ -1,18 +1,31 @@
 package com.example.mysweethome;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.amplifyframework.core.Amplify;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class AddHome extends AppCompatActivity {
 
     String selected;
+   // private EditText titleText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +58,44 @@ public class AddHome extends AppCompatActivity {
             public void onNothingSelected(AdapterView <?> parent) {
             }
         });
-        System.out.println("---------------+"+ selected);
+
+        Button upload=findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickFile=new Intent(Intent.ACTION_GET_CONTENT);
+                pickFile.setType("*/*");
+                pickFile=Intent.createChooser(pickFile,"Pick a Photo");
+                startActivityForResult(pickFile,5050);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String key=titleText.getText().toString();
+        File exampleFile = new File(getApplicationContext().getFilesDir(), "title");
+        try {
+            InputStream inputStream=getContentResolver().openInputStream(data.getData());
+            OutputStream outputStream=new FileOutputStream(exampleFile);
+            byte[]buf=new byte[1024];
+            int len;
+            while ((len=inputStream.read(buf))>0){
+                outputStream.write(buf,0,len);
+            }
+            inputStream.close();
+            outputStream.close();
+
+        } catch (Exception exception) {
+            Log.e("MyAmplifyApp", "Upload failed", exception);
+        }
+
+        Amplify.Storage.uploadFile(
+                key,
+                exampleFile,
+                result -> Log.i("MyAmplifyApp", "Successfully uploaded: " + result.getKey()),
+                storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure)
+        );
     }
 }
