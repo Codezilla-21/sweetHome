801f7619dfdfc406631df07961a81e5670b9465f
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-24 23:34:02 +0300

    add p1

--- app/src/main/java/com/example/mysweethome/AddHome.java
+++ app/src/main/java/com/example/mysweethome/AddHome.java
@@ -3,12 +3,48 @@ package com.example.mysweethome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AddHome extends AppCompatActivity {
+
+    String selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_home);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        Spinner spinner = findViewById(R.id.type);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Apartment");
+        arrayList.add("Villa");
+        arrayList.add("Flat");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tutorialsName = parent.getItemAtPosition(position).toString();
+                selected=tutorialsName;
+                System.out.println("---------------+"+ selected);
+                //  Toast.makeText(parent.getContext(), "Selected: " + tutorialsName, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
+        System.out.println("---------------+"+ selected);
     }
 }
--- app/src/main/java/com/example/mysweethome/MainActivity.java
+++ app/src/main/java/com/example/mysweethome/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.mysweethome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
@@ -15,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addHome = findViewById(R.id.addHome);
+        addHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAddHome = new Intent(MainActivity.this, AddHome.class);
+                startActivity(goToAddHome);
+            }
+        });
     }
 }
