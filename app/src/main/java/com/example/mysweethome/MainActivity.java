11054b5d630321a669956c533c6df006b35a11b6
shathasmadi <alsmadishatha@gmail.com>
2021-09-25 14:12:17 +0300

    finish main page style

--- app/src/main/java/com/example/mysweethome/MainActivity.java
+++ app/src/main/java/com/example/mysweethome/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.mysweethome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
@@ -13,7 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
 
-
+    ImageView imageView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView=findViewById(R.id.bottomNavView);
-
         navView.setBackground(null);
+
+
+        imageView=findViewById(R.id.logButton);
+        imageView.setClickable(true);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goTOSignIn =new Intent(MainActivity.this,Login.class);
+                startActivity(goTOSignIn);
+            }
+        });
+
+       String extras = getIntent().getStringExtra("userName");
+        TextView userName = findViewById(R.id.textView2);
+        if (extras != null){
+            userName.setText(extras);
+        }
 
     }
 }
