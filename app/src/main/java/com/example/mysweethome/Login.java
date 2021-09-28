5947e9c9c0686556c904d63cc9b7f04eebbbf75c
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-28 03:32:15 +0300

    forget password halfway

--- app/src/main/java/com/example/mysweethome/Login.java
+++ app/src/main/java/com/example/mysweethome/Login.java
@@ -91,6 +91,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, ForgetPassword.class);
+                intent.putExtra("userName",email.getText().toString());
                 startActivity(intent);
             }
         });
